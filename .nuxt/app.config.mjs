06eaa6f9bd5cd719391ba3c6c3fdc46fe9032942
Tomas Kmet <36038611+kmettom@users.noreply.github.com>
2023-02-27 18:58:06 +0000

    Delete .nuxt directory

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/home/tomas/Documents/Projects/NuxtThreeJSStartingPack/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/tomas/Documents/Projects/NuxtThreeJSStartingPack/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
