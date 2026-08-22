6c4edc2a9af31ee81dacff1f777db8b02fc6f55f
kmettom <kmettom@gmail.com>
2026-08-22 19:39:16 +0200

    playground - scroll performance - scroll speed bar

--- pages/playground/scroll-performance/index.vue
+++ pages/playground/scroll-performance/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="page-container">
+    <div
+      id="fixedParent"
+      v-canvas3-scroll-action="example3ScrollOptions"
+      class="scroll-speed-container"
+    >
+      <div class="scroll-speed-status-bar">
+        Scroll speed: {{ example3Speed }}
+        <span ref="scrollSpeedAniEl" class="scroll-speed-ani" />
+      </div>
+    </div>
     <div v-for="(slide, index) in slides" :key="slide.title" class="slide">
       <img
         v-canvas3-image="{ shaderName: 'example1' }"
@@ -17,6 +27,39 @@
   </div>
 </template>
 <script setup lang="ts">
+// TODO:
+// add scroll speed bar component (take from main page
+// adjust style? - take Jagoda's layout from a post
+// layout update
+// assets update
+// add appear animation with shader
+// add appear animation for text fields
+// adjust scroll shader
+// add appear animation for first blocks - text performance
+
+import gsap from "gsap";
+
+const scrollSpeedAniEl = ref<HTMLElement | null>(null);
+
+const example3Speed = ref(0);
+const example3ScrollOptions = computed(() => ({
+  activeRange: 0.9,
+  fixToParent: {
+    containerId: "fixedParent",
+    fixPosition: 0,
+    margin: 0,
+  },
+  onScrollCallback: scrollSpeedCallback,
+}));
+
+const scrollSpeedCallback = (_item: any, speed: number) => {
+  example3Speed.value = speed;
+  gsap.to(scrollSpeedAniEl.value, {
+    width: `${speed * 100}%`,
+    duration: 0.1,
+  });
+};
+
 const slides = ref([
   {
     title: "Slide 1",
@@ -66,4 +109,24 @@ onBeforeUnmount(() => {
   Canvas3.setScrollShaderByName("scrollPlayground");
 });
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.scroll-speed-container {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.scroll-speed-status-bar {
+  padding: 10px;
+}
+
+.scroll-speed-ani {
+  position: absolute;
+  height: 100%;
+  top: 0;
+  left: 0;
+  border: 1px solid red;
+}
+</style>
