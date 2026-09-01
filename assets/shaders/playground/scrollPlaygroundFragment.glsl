da1568a951086924c741bc819974d45b54264388
kmettom <kmettom@gmail.com>
2026-09-01 16:53:25 +0200

    playground - scroll performance - scroll shader adjust

--- assets/shaders/playground/scrollPlaygroundFragment.glsl
+++ assets/shaders/playground/scrollPlaygroundFragment.glsl
@@ -1,11 +1,19 @@
 uniform sampler2D tDiffuse;
 varying vec2 vUv;
 uniform float scrollSpeed;
+
 void main(){
     vec2 newUV = vUv;
-    float area = smoothstep(0.9, 0., vUv.y); // intensity 10%
-    area = pow(area, 4.);
-    newUV.x -= (vUv.x - 0.5) * 0.1 * area;
+
+    float scrollSpeedClamp = clamp(scrollSpeed / 0.1, 0.0, 1.0);
+
+    float distFromEdge = min(vUv.y, 1.0 - vUv.y) * scrollSpeedClamp;
+    //    float distFromEdge = min(vUv.y, 1.0 - vUv.y);
+    float area = smoothstep(0.9, 0.0, distFromEdge);
+    area = pow(area, 8.0);
+
+    float intensity = 0.10;
+    newUV.x -= (vUv.x - 0.5) * intensity * area;
 
     gl_FragColor = texture2D(tDiffuse, newUV);
 }
